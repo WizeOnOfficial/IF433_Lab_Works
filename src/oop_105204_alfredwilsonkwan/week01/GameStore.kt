cffd71979a7b0f5905caaeebe06252b9f506a1ef
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-02-02 16:50:40 +0700

    week01: (tugas 2) implement null safety for user note

--- src/oop_105204_alfredwilsonkwan/week01/GameStore.kt
+++ src/oop_105204_alfredwilsonkwan/week01/GameStore.kt
@@ -3,12 +3,14 @@ package oop_105204_alfredwilsonkwan.week01
 fun main() {
     val gameTitle = "Persona 5 Royal"
     val price = 540000
+    var userNote: String? = null
 
-    printReceipt(title = gameTitle, finalPrice = price)
+    printReceipt(title = gameTitle, finalPrice = price, userNote = userNote)
 }
 
 fun calculateDiscount(price: Int) = if(price > 500000) price - price * 0.2 else price - price * 0.1
 
-fun printReceipt(title: String, finalPrice: Int) {
+fun printReceipt(title: String, finalPrice: Int, userNote: String? = "Tidak ada catatan") {
     println("Game title: $title, Final price after Discount: ${calculateDiscount(finalPrice)}")
+    println("UserNote: $userNote")
 }
