971cff64b22920a3d5120aa75a2f6613d3b4c3ac
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-02-02 16:37:27 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_105204_alfredwilsonkwan/week01/GameStore.kt
+++ src/oop_105204_alfredwilsonkwan/week01/GameStore.kt
@@ -3,6 +3,12 @@ package oop_105204_alfredwilsonkwan.week01
 fun main() {
     val gameTitle = "Persona 5 Royal"
     val price = 540000
+
+    printReceipt(title = gameTitle, finalPrice = price)
 }
 
 fun calculateDiscount(price: Int) = if(price > 500000) price - price * 0.2 else price - price * 0.1
+
+fun printReceipt(title: String, finalPrice: Int) {
+    println("Game title: $title, Final price after Discount: ${calculateDiscount(finalPrice)}")
+}
