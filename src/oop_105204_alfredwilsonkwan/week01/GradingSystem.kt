504f00174eb20154d0563a9d9d66736c151c1a20
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-02-02 15:39:05 +0700

    week01: refactor to use val and string templates

--- src/oop_105204_alfredwilsonkwan/week01/GradingSystem.kt
+++ src/oop_105204_alfredwilsonkwan/week01/GradingSystem.kt
@@ -1,9 +1,10 @@
 package oop_105204_alfredwilsonkwan.week01
 
 fun main() {
-    var name: String = "John Thor"
-    var score: Int = 80
+    //REFACTOR: Gunakan val dan hapus tipe data eksplisit
+    val name = "John Thor"
+    val score = 80
 
-    //Concatenation gaya lama
-    println("Nama: " + name + ", Nilai: " + score)
+    //  REFACTOR: String Template ($name)
+    println("Nama: $name, Nilai: $score")
 }
