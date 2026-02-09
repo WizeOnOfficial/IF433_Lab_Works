9447b195b1a036c047c6a4938b831f24dd16cefa
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-02-09 16:09:10 +0700

    week02: (task 1) implement library input system

---
+++ src/oop_105204_alfredwilsonkwan/week02/LoanMain.kt
@@ -0,0 +1,27 @@
+package oop_105204_alfredwilsonkwan.week02
+
+import java.util.Scanner
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+
+    println("--- UMN Library System ---")
+
+    print("Input the Book's Title: ")
+    val bookTitle = scanner.nextLine()
+
+    print("Input borrower's name: ")
+    val borrower = scanner.nextLine()
+
+    print("Input loan duration: ")
+    var loanDuration = scanner.nextInt()
+    scanner.nextLine()
+
+    if(loanDuration < 0) {
+        loanDuration = 1
+    } else {
+        val report = Loan(bookTitle, borrower, loanDuration)
+        println("Report saved! Borrower's name: ${report.borrower}, Book title: ${report.bookTitle}, Loan duration: $loanDuration Day")
+        println("Final Total loan: ${report.calculateFine()}")
+    }
+}
