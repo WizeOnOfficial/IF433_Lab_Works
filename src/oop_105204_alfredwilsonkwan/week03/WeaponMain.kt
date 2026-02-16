e893568d410fc303b373a14caed9b6eb944a965d
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-02-16 15:26:45 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_105204_alfredwilsonkwan/week03/WeaponMain.kt
@@ -0,0 +1,11 @@
+package oop_105204_alfredwilsonkwan.week03
+
+fun main() {
+    val stick = Weapon()
+//    stick.damage = -50
+
+    val gun = Weapon()
+    gun.damage = 9999
+    println(gun.damage)
+    println(gun.tier)
+}
