02d16aab253002c22d26f3beca8118bb581e3dbb
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-02-27 23:54:59 +0700

    week04: (task 1) create ElectricCar class with final override

---
+++ src/oop_105204_alfredwilsonkwan/week04/ElectricCar.kt
@@ -0,0 +1,7 @@
+package oop_105204_alfredwilsonkwan.week04
+
+class ElectricCar(brand: String, numberOfDoors: Int, val batteryCapacity: Int) : Car(brand, numberOfDoors) {
+    final override fun accelerate() {
+        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%")
+    }
+}
