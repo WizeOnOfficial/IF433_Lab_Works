a5b24ea35071923b2d85ed430340a39a10187a69
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-02-28 00:00:35 +0700

    week04: (task 2) create parent class Employee

---
+++ src/oop_105204_alfredwilsonkwan/week04/Employee.kt
@@ -0,0 +1,11 @@
+package oop_105204_alfredwilsonkwan.week04
+
+open class Employee(val name: String, val baseSalary: Int) {
+    open fun work() {
+        println("$name sedang bekerja.")
+    }
+
+    open fun calculateBonus(): Int {
+        return (baseSalary * 0.1).toInt()
+    }
+}
