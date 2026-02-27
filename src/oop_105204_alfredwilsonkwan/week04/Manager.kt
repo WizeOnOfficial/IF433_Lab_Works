18c8d52a0d49812f23674cb3d604c2869bc17802
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-02-28 00:03:57 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_105204_alfredwilsonkwan/week04/Manager.kt
@@ -0,0 +1,11 @@
+package oop_105204_alfredwilsonkwan.week04
+
+class Manager(name: String, baseSalary: Int) : Employee(name, baseSalary) {
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 500000
+    }
+}
