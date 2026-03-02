be3588d8cddee621bab793e3caf4ef9756086dd8
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-03-02 15:38:34 +0700

    week05: (task 1) test MathHelper overloading in main AND fix class MathHelper

---
+++ src/oop_105204_alfredwilsonkwan/week05/MathHelperMain.kt
@@ -0,0 +1,12 @@
+package oop_105204_alfredwilsonkwan.week05
+
+fun main() {
+    val square = hitungLuas(20)
+    val rectangle = hitungLuas(15, 30)
+    val circle = hitungLuas(14.0)
+
+    println("\n --- Math Helper ---")
+    println("Square = $square")
+    println("Rectangle = $rectangle")
+    println("Circle = $circle")
+}
