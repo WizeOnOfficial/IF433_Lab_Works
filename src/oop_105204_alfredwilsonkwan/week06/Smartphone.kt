8ea8d9f436fc26f43d506d141eafb9b2f9145d38
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-03-09 12:26:54 +0700

    week06: fix diamond problem using explicit super delegation

--- src/oop_105204_alfredwilsonkwan/week06/Smartphone.kt
+++ src/oop_105204_alfredwilsonkwan/week06/Smartphone.kt
@@ -1,4 +1,9 @@
 package oop_105204_alfredwilsonkwan.week06
 
 class Smartphone : Camera, Phone {
+    override fun turnOn() {
+        super<Camera>.turnOn()
+        super<Phone>.turnOn()
+        println("Sistem operasi Smartphone berhasil booting.")
+    }
 }
