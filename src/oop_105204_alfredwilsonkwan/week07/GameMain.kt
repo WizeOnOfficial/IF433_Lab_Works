7cfcf1e22c5ca0956683ccbadc4ef9aed0bf9282
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-03-16 17:51:06 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_105204_alfredwilsonkwan/week07/GameMain.kt
+++ src/oop_105204_alfredwilsonkwan/week07/GameMain.kt
@@ -3,4 +3,11 @@ package oop_105204_alfredwilsonkwan.week07
 fun main() {
     GameManager.startGame()
     GameManager.startGame()
+
+    println("\n=== LOOT RARITY CHECK ===")
+    println("Legendary drop chance adalah: ${ItemRarity.LEGENDARY.dropChance}")
+
+    println("\n=== FORGE CHECK ===")
+    val mySword = Weapon.forgeStarterSword()
+    println(mySword.item)
 }
