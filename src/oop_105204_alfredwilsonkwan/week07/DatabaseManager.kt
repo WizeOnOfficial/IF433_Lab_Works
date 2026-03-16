8264b9b1fbc75882e919cefc977a0b07ba4772b5
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-03-16 15:35:35 +0700

    week07: create singleton DatabaseManager

---
+++ src/oop_105204_alfredwilsonkwan/week07/DatabaseManager.kt
@@ -0,0 +1,10 @@
+package oop_105204_alfredwilsonkwan.week07
+
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect() {
+        connectionStatus = "Connected to Server"
+        println("Database is ready.")
+    }
+}
