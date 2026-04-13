33bbeb05636906534dd9629090218d9ad1631e30
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-04-13 13:50:27 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_105204_alfredwilsonkwan/week08/Main.kt
+++ src/oop_105204_alfredwilsonkwan/week08/Main.kt
@@ -20,8 +20,15 @@ fun main() {
     val mixedData: List<Any> = listOf(
         "Smartphone",
         15000000,
-        UserProfile("Andi", null)
+        UserProfile("Andi", null),
         "Laptop",
         4500000.0
     )
+
+    for (item in mixedData) {
+        val text = item as? String
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
