6a19fdd1ce72b32f0a2895fd5a6ab04065b27ac6
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-04-20 15:13:27 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_105204_alfredwilsonkwan/week09/TradingModels.kt
@@ -0,0 +1,10 @@
+package oop_105204_alfredwilsonkwan.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
+
