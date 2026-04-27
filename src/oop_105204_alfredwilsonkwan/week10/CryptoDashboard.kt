7c7704e6a06f5909ff831ff9525d1668a598e1db
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-04-27 15:40:22 +0700

    week10: (task) populate coin repository

--- src/oop_105204_alfredwilsonkwan/week10/CryptoDashboard.kt
+++ src/oop_105204_alfredwilsonkwan/week10/CryptoDashboard.kt
@@ -2,4 +2,14 @@ package oop_105204_alfredwilsonkwan.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
+    val BTC = Coin("BTC", 200000.0)
+    val ETH = Coin("ETH", 125000.0)
+    val USDT = Coin("USDT", 345000.0)
+
+    coinRepo.add(BTC)
+    coinRepo.add(ETH)
+    coinRepo.add(USDT)
+
+
 }
