00f67818603d14e028a011fcc2f3129103bdab15
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-05-04 12:56:15 +0700

    week11: create string extension functions

---
+++ src/oop_105204_alfredwilsonkwan/week11/StringExtensions.kt
@@ -0,0 +1,9 @@
+package oop_105204_alfredwilsonkwan.week11
+
+fun String.addGreeting(): String {
+    return "Hello, $this"
+}
+
+fun String.repeatTimes(n: Int): String {
+    return this.repeat(n)
+}
