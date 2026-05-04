3327c973e003eb91d71f20bae00f27d07f0f1e4c
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-05-04 15:16:24 +0700

    week11: (task) implement device search with let

--- src/oop_105204_alfredwilsonkwan/week11/SmartHomeDashboard.kt
+++ src/oop_105204_alfredwilsonkwan/week11/SmartHomeDashboard.kt
@@ -28,4 +28,11 @@ fun main() {
     homeDevices.add(acInverter)
     homeDevices.add(SmartDevice("Picolo's Auto Feeder","Pet Care", true, 10))
 
+    val searchResult = homeDevices.find {
+        it.category == "Camera"
+    }
+    searchResult?.let {
+        println("\n=== Hasil Pencarian Kamera ===")
+        println(it.diagnose())
+    }
 }
