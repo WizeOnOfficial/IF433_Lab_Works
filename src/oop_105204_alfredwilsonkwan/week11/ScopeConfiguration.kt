f40ed03d6db447544eadcf6f087aec89f64faf0e
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-05-04 13:11:51 +0700

    week11: implement apply for object initialization

--- src/oop_105204_alfredwilsonkwan/week11/ScopeConfiguration.kt
+++ src/oop_105204_alfredwilsonkwan/week11/ScopeConfiguration.kt
@@ -1,3 +1,13 @@
 package oop_105204_alfredwilsonkwan.week11
 
 data class User(var name: String = "", var age: Int = 0)
+
+fun main() {
+    println("=== TEST APPLY ===")
+    val user = User().apply {
+        name = "Alex"
+        age = 25
+    }
+
+    println(user)
+}
