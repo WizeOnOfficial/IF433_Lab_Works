6d41dd80ceaf8390a6bf45a9dea7800d086238f6
WizeOnOfficial <wizeongaming2019@gmail.com>
2026-05-11 15:36:53 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_105204_alfredwilsonkwan/week12/SmartFeeder.kt
+++ src/oop_105204_alfredwilsonkwan/week12/SmartFeeder.kt
@@ -35,5 +35,8 @@ fun main() {
 
     runCatching {
         dispenseKibble(30, 1000, false)
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }
 }
