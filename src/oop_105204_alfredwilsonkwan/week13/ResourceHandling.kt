dd9206ac84b421a0f724dca1d0334d7965ec294b
WZ. <87628814+WizeOnOfficial@users.noreply.github.com>
2026-05-18 13:02:03 +0700

    week13: implement manual close for file stream

---
+++ src/oop_105204_alfredwilsonkwan/week13/ResourceHandling.kt
@@ -0,0 +1,20 @@
+package oop_105204_alfredwilsonkwan.week13
+import java.io.File
+
+fun main() {
+    println("=== TEST UNSAFE RESOURCE HANDLING ===")
+    val unsafeFile = File("unsafe_logs.txt")
+    val writer = unsafeFile.printWriter()
+
+    writer.println("Log 1: Membuka koneksi database...")
+    writer.println("Log 2: Menulis data pengguna...")
+
+    // BAHAYA: Jika terjadi Exception di baris ini (misalnya pembagian dengan nol atau error tak terduga),
+    // program akan crash dan metode writer.close() di bawahnya TIDAK AKAN PERNAH TEREKSEKUSI!!
+    // File akan terus terkunci oleh OS.
+
+    // Wajib dipanggil secara manuall jika tidak memakai blok 'use'
+    writer.close()
+    println("Proses penulisan unsafe selesai.")
+
+}
