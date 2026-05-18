f318759f0f0952c8f27c0105f88106ce586d9911
WZ. <87628814+WizeOnOfficial@users.noreply.github.com>
2026-05-18 14:32:42 +0700

    week13: (task) implement saveTrades using use block and printWriter

--- src/oop_105204_alfredwilsonkwan/week13/CryptoTradeLogger.kt
+++ src/oop_105204_alfredwilsonkwan/week13/CryptoTradeLogger.kt
@@ -1,4 +1,5 @@
 package oop_105204_alfredwilsonkwan.week13
+import java.io.File
 
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
 
@@ -14,5 +15,13 @@ fun fromCsvTrade(line: String): TradeRecord? {
     } catch (e: IndexOutOfBoundsException) {
         println("(Log) Dat korup diabaikan: $line")
         return null
+    }
+}
+
+fun saveTrades(trades: List<TradeRecord>, path: String) {
+    File(path).printWriter().use { out ->
+        trades.forEach {
+            out.println(it.toCsv())
+        }
     }
 }
